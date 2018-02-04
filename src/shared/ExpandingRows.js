664686e990cdfbd7c14d847f28af4e7697915678
Jeremy Wood <farachan@gmail.com>
2018-02-04 00:38:08 -0500

    Made ExpandingRows expand without creating clicks.

--- src/shared/ExpandingRows.js
+++ src/shared/ExpandingRows.js
@@ -1,41 +1,78 @@
 import React from 'react';
 import mergeProps from './mergeProps';
 
-const getCustomTdProps = () => ({
-  onClick: (e, handleOriginal) => {
-    e.target.parentNode.firstChild.click();
-
-    // IMPORTANT! React-Table uses onClick internally to trigger
-    // events like expanding SubComponents and pivots.
-    // By default a custom 'onClick' handler will override this functionality.
-    // If you want to fire the original onClick handler, call the
-    // 'handleOriginal' function.
-    if (handleOriginal) {
-      handleOriginal();
-    }
-  },
-});
-
 export default function expandingRows(WrappedReactTable) {
-  const ExpandableRowsReactTable = (props) => {
-    const newProps = Object.assign({}, props);
-    const getTdProps = newProps.getTdProps;
-
-    let newGetTdProps;
-    if (getTdProps) {
-      newGetTdProps = mergeProps(getCustomTdProps, getTdProps);
-      delete newProps.getTdProps;
-    } else {
-      newGetTdProps = getCustomTdProps;
+  class ExpandableRowsReactTable extends React.Component {
+    constructor(props) {
+      super(props);
+      this.getCustomTrProps = this.getCustomTrProps.bind(this);
+      this.onExpandedChange = this.onExpandedChange.bind(this);
+      this.onSortedChange = this.onSortedChange.bind(this);
+      this.onFilteredChange = this.onFilteredChange.bind(this);
+      this.onPageChange = this.onPageChange.bind(this);
+      this.state = {
+        expanded: {},
+      };
     }
 
-    return (
-      <WrappedReactTable
-        getTdProps={newGetTdProps}
-        {...newProps}
-      />
-    );
-  };
+    onExpandedChange(expanded) {
+      this.setState({ expanded });
+    }
+
+    onSortedChange() {
+      this.setState({
+        expanded: {},
+      });
+    }
+
+    onFilteredChange() {
+      this.setState({
+        expanded: {},
+      });
+    }
+
+    onPageChange() {
+      this.setState({
+        expanded: {},
+      });
+    }
+
+    getCustomTrProps(state, rowInfo) {
+      return {
+        onClick: () => {
+          const expanded = Object.assign({}, this.state.expanded);
+          expanded[rowInfo.viewIndex] = !this.state.expanded[rowInfo.viewIndex];
+          this.setState({ expanded });
+        },
+      };
+    }
+
+    render() {
+      const newProps = Object.assign({}, this.props);
+      const getTdProps = newProps.getTrProps;
+
+      let newGetTrProps;
+      if (getTdProps) {
+        newGetTrProps = mergeProps(this.getCustomTrProps, getTdProps);
+        delete newProps.getTrProps;
+      } else {
+        newGetTrProps = this.getCustomTrProps;
+      }
+
+      return (
+        <WrappedReactTable
+          expanded={this.state.expanded}
+          onExpandedChange={this.onExpandedChange}
+          onSortedChange={this.onSortedChange}
+          onFilteredChange={this.onFilteredChange}
+          onPageChange={this.onPageChange}
+          getTrProps={newGetTrProps}
+          {...newProps}
+        />
+      );
+    }
+  }
+
   ExpandableRowsReactTable.propTypes = WrappedReactTable.propTypes;
   return ExpandableRowsReactTable;
 }
